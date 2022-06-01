ba69eb11159a05dc92af6ec526786225d3063a89
Alan-Bushell <alanbushell91@gmail.com>
2022-06-01 13:40:04 +0000

    testing auto deploy to heroku

--- run.py
+++ run.py
@@ -1,1 +1,13 @@
-print("Connected to Heroku and ready to start coding: ['♠', '♦', '♥', '♣']")
+"""
+Python Blackjack command line game for porfolio 3 project
+"""
+
+
+def start():
+    """
+    Initial function to test connection to heroku
+    """
+    print("Connected to Heroku and ready to start coding:['♠', '♦', '♥', '♣']")
+
+
+start()
