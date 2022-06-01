b0aba8d37025639591b01d2dab710ec63e7eec6b
Alan-Bushell <alanbushell91@gmail.com>
2022-06-01 14:11:06 +0000

    add welcome banner and name input to program@

--- run.py
+++ run.py
@@ -7,7 +7,20 @@ def start():
     """
     Initial function to test connection to heroku
     """
-    print("Connected to Heroku and ready to start coding:['♠', '♦', '♥', '♣']")
+    print("\n                               Welcome to:")
+    banner = """
+.------..------..------..------..------..------..------..------..------.
+|B.--. ||L.--. ||A.--. ||C.--. ||K.--. ||J.--. ||A.--. ||C.--. ||K.--. |
+| :(): || :/\: || (\/) || :/\: || :/\: || :(): || (\/) || :/\: || :/\: |
+| ()() || (__) || :\/: || :\/: || :\/: || ()() || :\/: || :\/: || :\/: |
+| '--'B|| '--'L|| '--'A|| '--'C|| '--'K|| '--'J|| '--'A|| '--'C|| '--'K|
+`------'`------'`------'`------'`------'`------'`------'`------'`------'
+"""
+    print(banner)
+    print("By Alan Bushell - Portfolio 3\n")
+    print("Good luck and try not to lose to much!\n")
+    name = input("To start, please enter your name:\n")
+    print(f"Welcome {name}. Get ready to be dealt your cards!")
 
 
 start()
