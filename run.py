f4da08710ba13369ef3c3c69f1dcbd6a92922ccb
Alan-Bushell <alanbushell91@gmail.com>
2022-06-12 19:20:32 +0000

    Add main menu options and finish gameflow

--- run.py
+++ run.py
@@ -2,24 +2,71 @@ import random  # import random to shuffle deck of cards
 
 # Move card values into global scope
 
-suits = ["♠", "♥", "♣", "♦"]
+suits = ["♠", "♥", "♣", "♦"]  # Took characters from computer keyboard
 ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q",
          "K", "A"]
 values = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7,
           "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10, "A": 11}
 
+
+BANNER = """
+    .------..------..------..------..------..------..------..------..------.
+    |B.--. ||L.--. ||A.--. ||C.--. ||K.--. ||J.--. ||A.--. ||C.--. ||K.--. |
+    | :(): || :/\: || (\/) || :/\: || :/\: || :(): || (\/) || :/\: || :/\: |
+    | ()() || (__) || :\/: || :\/: || :\/: || ()() || :\/: || :\/: || :\/: |
+    | '--'B|| '--'L|| '--'A|| '--'C|| '--'K|| '--'J|| '--'A|| '--'C|| '--'K|
+    `------'`------'`------'`------'`------'`------'`------'`------'`------'
+"""
+
+print("\n                              Welcome to:")
+print(BANNER)
+
+# Input for users name and to store it
 name = input("To start, please enter your name:\n")
+print(f"Welcome to the game {name}.")
+
+# Input option for rules or to start game
+print("\nPlease select one of following options:")
+main_choice = input("\n1.Start Game\n2.Read Rules\nChoice:\n")
+
+# Test input to ensure it is valis
 
 
-hidden_card = """
+if main_choice == 1:
+    PLAYING = True
+
+else:
+    PLAYING = False
+    print("""
+    Rules of BlackJack \n
+    1. The player and dealer are both dealt two cards \n
+    2. The player will get both cards face up and the dealer
+    will have one card hidden. \n
+    3. The goal of the game is to get as close to 21 without going over \n
+    4. You will be given the option to Hit or Stand during your turn \n
+    5. Once you are satisfied with your score and stand then its the 
+    dealers turn. \n
+    6. The dealer must hit and draw a card on 16 but stand on 17\n
+    7. If the dealer goes over 21 he loses and you win.\n
+    8. If both yourself and the dealer do not exceed 21 then\n
+    the closest to 21 wins\n
+    9. Good luck
+    """)
+
+rules_choice = input("When you are ready to play press: 'Y'\n")
+
+if rules_choice.lower == 'y':
+    PLAYING = True
+else:
+    print("I am sorry but that option is not recognized. Try Again")
+
+HIDDEN_CARD = """
 .-------.
 |?.---. |
 | : ? : |
 | '---'?|
 `-------'
         """
-
-PLAYING = True
 
 
 class Card:
@@ -134,10 +181,10 @@ def first_round(player, dealer):
     dealers card
     """
     print("Dealers Hand:")
-    print(hidden_card)
+    print(HIDDEN_CARD)
     print('', dealer.cards[1])
     print("\nPlayers Hand:", *player.cards, sep="\n")
-    print("\nPlayers Total = ", player.value)
+    print("\nPlayers Score = ", player.value)
 
 
 def display_all_cards(player, dealer):
@@ -147,9 +194,9 @@ def display_all_cards(player, dealer):
     be calculated
     """
     print("Dealers Hand:", *dealer.cards, sep="\n")
-    print("Dealer's Hand =", dealer.value)
+    print("Dealer's Score =", dealer.value)
     print("Player's Hand:", *player.cards, sep="\n")
-    print("\nPlayers Total = ", player.value)
+    print("\nPlayers Score = ", player.value)
 
 
 def player_wins(player, dealer):
@@ -190,16 +237,6 @@ def push(player, dealer):
 # Starting the game
 
 while True:
-    print("\n                              Welcome to:")
-    BANNER = """
-    .------..------..------..------..------..------..------..------..------.
-    |B.--. ||L.--. ||A.--. ||C.--. ||K.--. ||J.--. ||A.--. ||C.--. ||K.--. |
-    | :(): || :/\: || (\/) || :/\: || :/\: || :(): || (\/) || :/\: || :/\: |
-    | ()() || (__) || :\/: || :\/: || :\/: || ()() || :\/: || :\/: || :\/: |
-    | '--'B|| '--'L|| '--'A|| '--'C|| '--'K|| '--'J|| '--'A|| '--'C|| '--'K|
-    `------'`------'`------'`------'`------'`------'`------'`------'`------'
-"""
-    print(BANNER)
 
     deck = Deck()  # Initialise deck
     deck.shuffle()  # Shuffle decl
@@ -236,10 +273,10 @@ while True:
         if dealer_hand.value > 21:
             dealer_busts(player_hand, dealer_hand)
 
-        elif player_hand < 21 and player_hand > dealer_hand:
+        elif player_hand.value > dealer_hand.value:
             player_wins(player_hand, dealer_hand)
 
-        elif player_hand < dealer_hand:
+        elif player_hand.value < dealer_hand.value:
             dealer_wins(player_hand, dealer_hand)
 
         else:
@@ -252,6 +289,7 @@ while True:
 
         continue
     else:
+        PLAYING = False
         print("""
     .------..------..------..------..------.     .------..------..------.
     |T.--. ||H.--. ||A.--. ||N.--. ||K.--. |     |Y.--. ||O.--. ||U.--. |
